61156d9440f2988b22a6be31b21b90cedf78fbea
Richard <leeroydevassistant2@gmail.com>
2020-11-16 18:38:58 +0100

    :truck:setup to work in vuejs

--- src/index.js
+++
@@ -1,2 +0,0 @@
-import './assets/styles/main.scss'
-import './assets/js/account_access.js'
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <h2>Test</h2>
+    <img alt="Vue logo" src="../assets/images/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<style lang="scss"></style>
